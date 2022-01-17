b6fb7611053406c7992920d05660d61fe83d06e3
louannem <84766775+louannem@users.noreply.github.com>
2022-01-17 23:37:33 +0100

    Ajout des données dans RadialChart

--- src/components/RadialChart/index.jsx
+++ src/components/RadialChart/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { RadialBarChart, RadialBar, ResponsiveContainer, PolarAngleAxis } from 'recharts';
+import { fetchUSerData } from "../../utils/service/Service";
 
-const data = [
+const test = [
     {
         subject:'score',
         value: 12,
-        totalScore: 100,
         fill: '#ff0000'
     }
 ]
@@ -25,13 +25,28 @@ function CustomLabel({viewBox, value1, value2}){
 
 
 class SimpleRadialChart extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [{score:'', fill:"#ff0000"}]
+        }
+      } 
+
+      async componentDidMount(){ 
+        const user = await fetchUSerData()
+        console.log(user.score)
+        this.setState({ 
+            data:[{score: user.score*100, fill:"#ff0000"}]
+        })
+      }
+
     render(){
         return(
             <ResponsiveContainer width="100%" height="100%">
-                <RadialBarChart startAngle={-270} endAngle={90} cx="50%" cy="50%" innerRadius="70%" outerRadius="80%" barSize={10} data={data}>
-                    <PolarAngleAxis type="number" domain={[0, data[0].totalScore]} tick={false}  />
-                    <RadialBar cornerRadius={10} minAngle={15} label={<CustomLabel value1={data[0].value + "%"} value2="de votre objectif" />} clockWise dataKey="value" />
-                    
+                <RadialBarChart startAngle={-270} endAngle={90} cx="50%" cy="50%" innerRadius="70%" outerRadius="80%" barSize={10} data={this.state.data}>
+                    <PolarAngleAxis type="number" domain={[0, 100]} tick={true}  />
+                    <RadialBar cornerRadius={10} minAngle={15} label={<CustomLabel value1={this.state.data[0].score + "%"} value2="de votre objectif" />} clockWise dataKey="score" />
                 </RadialBarChart>
             </ResponsiveContainer>
         )
--- src/utils/service/Service.js
+++ src/utils/service/Service.js
@@ -5,3 +5,11 @@ export const fetchUSerData = () => {
     .catch((error) => console.log(error))
     )
 }
+
+export const fetchPerfData = () => {
+    return fetch(`http://localhost:3000/user/12/performance`)
+    .then((response) => response.json()
+    .then((user) => user.data )
+    .catch((error) => console.log(error))
+    )
+}
